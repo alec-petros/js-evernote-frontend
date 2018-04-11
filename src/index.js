09a53677f6d20202b76a8fca13399052e26218e4
alec-petros <a.petros@verizon.net>
2018-04-11 13:41:17 -0400

    its broke

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-document.addEventListener('ready', () => {
-  const appContent = $('#app-content')
-  const url = "http://localhost:3000/api/v1/"
-  document.getElementById('test').innerText = "hello"
-  fetch(url+'notes').then(r => r.json()).then(r => console.log(r))
+const appContent = $('#app-content')
+const url = "http://localhost:3000/api/v1/"
+document.getElementById('test').innerText = "hello"
+fetch(url+'notes').then(r => r.json()).then(r => {
+  r.map(note => {
+    document.createElement('div')
+  })
 })
+
+function renderNote(note) {
+
+}
